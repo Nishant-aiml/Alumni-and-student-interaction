1e8134a6bb9fe617219d887b63e37e195f8ebfd5
Nishant Datta <dattanishant2@gmail.com>
2025-01-21 10:39:33 +0530

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ import Rewards from './pages/Rewards';
 import Jobs from './pages/Jobs';
 import Feeds from './pages/Feeds';
 import Resources from './pages/Resources';
-import UniversalChatbot from './components/universal/UniversalChatbot';
 
 // Layout for authenticated pages
 const AuthenticatedLayout = ({ children }: { children: React.ReactNode }) => {
@@ -48,7 +47,6 @@ const AuthenticatedLayout = ({ children }: { children: React.ReactNode }) => {
           {children}
         </div>
       </main>
-      <UniversalChatbot />
       <InstallPrompt />
     </div>
   );
